07b91fe08877af5b84d6a9928765d05b6c57fccd
Wellington Siqueira Porto <dihhhd71@icloud.com>
2025-08-27 18:05:51 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,93 @@
+
+class Restaurante:
+
+    def __init__(self, produto, bebida, acompanhamento):
+        self.produto = produto
+        self.bebida = bebida
+        self.acompanhamento = acompanhamento
+
+
+    def hamburgueria():
+        self.produto = "hamburguer"
+        self,bebida = "refri"
+        self.acompanhamento = "fritas"
+
+    def temakeria():
+        self.produto = "temaki"
+        self.bebida = "suco"
+        self.acompanhamento = "sushi"
+
+
+    def pizzaria():
+        self.produto = "pizza"
+        self.bebida = "coca-cola"
+        self.acompanhamento = "esfiha"
+
+
+class Item_cardapio:
+    def __init__(self ):
+
+
+class pedido:
+    TAXA_DE_ENTREGA_RAPIDA = 5
+
+    def __init__(self_, nome: str, preco: float, status):
+        self.nome= none 
+        self.preco = preco
+        self.status = status
+
+
+
+
+class Pagamento:
+
+    def __init__(self, debito, credito, dinheiro):
+        self.debito = debito
+        self.credito = credito
+        self.dinheiro = dinheiro
+
+        def valor_total
+    
+    
+    def Forma_de_pagamento(self):
+        tipo_pagamento = input()
+        if tipo_pagamento == "debito":
+            print(f"{valor_total} foi pago")
+        elif(tipo_pagamento == "credito"):
+            print(f"compra aprovada no credito com valor total de {valor_total}") 
+        elif(tipo_pagamento == "dinheiro"):
+            print(f"valor a ser pago na hora ")
+
+
+
+
+h = Pagamento.Forma_de_pagamento()
+
+print(h)
+
+
+# class Pizzaria:
+#     def __init__(self, pizza, refri, esfiha):
+#         self.pizza = pizza
+#         self.refri = refri
+#         self.esfiha = esfiha
+
+
+# class Hamburgueria:
+#     def __init__(self, hamburguer, refri, batata):
+#         self.hamburguer = hamburguer
+#         self.refri = refri
+#         self.batata = batata
+
+#     def criar():
+#         hamburguer = "monster",    
+#         refri = "coca"
+#         batata = "fritas"
+
+# class Temakeria:
+#     def __init__(self, temaki, refri, sushi):
+#         self.temaki = temaki
+#         self.refri = refri
+#         self.sushi = sushi
+
+
